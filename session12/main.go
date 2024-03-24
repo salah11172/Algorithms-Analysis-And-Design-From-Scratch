0d94e9852ca0252b446a8f0d057d874aab54fb61
salah11172 <salahmustafa11172@gmail.com>
2024-03-24 22:22:07 +0200

    Binary Search

---
+++ session12/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func mergeSort(arr []int) []int {
+	length := len(arr)
+
+	if length <= 1 {
+		return arr
+	}
+
+	middle := length / 2
+	left := mergeSort(arr[:middle])
+	right := mergeSort(arr[middle:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	leftIndex, rightIndex := 0, 0
+
+	for leftIndex < len(left) && rightIndex < len(right) {
+		if left[leftIndex] < right[rightIndex] {
+			result = append(result, left[leftIndex])
+			leftIndex++
+		} else {
+			result = append(result, right[rightIndex])
+			rightIndex++
+		}
+	}
+
+	result = append(result, left[leftIndex:]...)
+	result = append(result, right[rightIndex:]...)
+
+	return result
+}
+
+func main() {
+	arr := []int{6, 16, 13, 5, 1, 7, 20, 3, 11}
+	fmt.Println("Unsorted array:", arr)
+	sorted := mergeSort(arr)
+	fmt.Println("Sorted array:", sorted)
+}
